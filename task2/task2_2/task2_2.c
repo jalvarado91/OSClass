e797616ea5ea69326efcde6dfb77692a04b6deba
Juan Alvarado <jalvarado91@gmail.com>
2017-07-09 12:43:52 -0400

    task 2.2

--- task2/task2_2/task2_2.c
+++ task2/task2_2/task2_2.c
@@ -7,41 +7,43 @@
 
 int main(int argc, char *argv[])
 {
-    int nthreads, tid, i, chunk;
-    float a[N], b[N], c[N];
-    double start_time, end_time, elapsed_time;
+	int nthreads, tid, i, chunk;
+	float a[N], b[N], c[N];
+	double start_time, end_time, elapsed_time;
 
-    for (i = 0; i < N; i++)
-        a[i] = b[i] = i * 1.0; // initialize arrays
+	for (i = 0; i < N; i++)
+		a[i] = b[i] = i * 1.0; // initialize arrays
 
-    chunk = CHUNKSIZE;
+	chunk = CHUNKSIZE;
 
-    printf("Program starting...\n",);
-    start_time = omp_get_wtime();
-    printf("Start time: %d, \n", start_time);
+	printf("Program starting.\n");
+	start_time = omp_get_wtime();
+	printf("Start time: %d.\n", start_time);
 
-    #pragma omp parallel shared(a, b, c, nthreads, chunk) private(i, tid) {
-        tid = omp_get_thread_num();
 
-        if (tid == 0)
-        {
-            nthreads = omp_get_num_threads();
-            printf("Number of threads = %d\n", nthreads);
-        }
+	#pragma omp parallel shared(a, b, c, nthreads, chunk) private(i, tid) 
+	{
+		tid = omp_get_thread_num();
 
-        printf("Thread %d starting...\n", tid);
+		if (tid == 0)
+		{
+			nthreads = omp_get_num_threads();
+			printf("Number of threads = %d\n", nthreads);
+		}
 
-        #pragma omp for schedule(dynamic, chunk)
-        for (i = 0; i < N; i++)
-        {
-            c[i] = a[i] + b[i];
-            printf("Thread %d: c[%d]= %f\n", tid, i, c[i]);
-        }
+		printf("Thread %d starting...\n", tid);
 
-    } /* end of parallel section */
+		#pragma omp for schedule(static, chunk)
+		for (i = 0; i < N; i++)
+		{
+			c[i] = a[i] + b[i];
+			printf("Thread %d: c[%d]= %f\n", tid, i, c[i]);
+		}
 
-    end_time = omp_get_wtime();
-    printf("End time: %d, \n", end_time);
-    elapsed_time = end_time - start_time;
-    printf("Total elapsed time: %d\n", elapsed_time);
+	} /* end of parallel section */
+
+	end_time = omp_get_wtime();
+	printf("End time: %d. \n", end_time);
+	elapsed_time = end_time - start_time;
+	printf("Total elapsed time: %d.\n", elapsed_time);
 }

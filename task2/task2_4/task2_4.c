5da08b2af65c762af2a760bdbfc1df35e9d5acdd
Juan Alvarado <jalvarado91@gmail.com>
2017-07-09 13:24:43 -0400

    tasks 2.3 2.4

---
+++ task2/task2_4/task2_4.c
@@ -0,0 +1,73 @@
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define M 500
+#define N 500
+
+int num_threads;
+
+int main(int argc, char *argv)
+{
+    // Input Validation
+	if (argc <= 1)
+	{
+		printf("Error: Please specify a number threads.\n");
+		return 0;
+	}
+
+	num_threads = atoi(argv[1]);
+	if (!num_students)
+	{
+		printf("Arguments are not valid.\n");
+		return 0;
+	}
+    //set number of threads here
+    omp_set_num_threads(num_threads);
+
+    int i, j, k;
+    double sum;
+    double **A, **B, **C;
+
+    A = malloc(M * sizeof(double *));
+    B = malloc(M * sizeof(double *));
+    C = malloc(M * sizeof(double *));
+
+    for (i = 0; i < M; i++)
+    {
+        A[i] = malloc(N * sizeof(double));
+        B[i] = malloc(N * sizeof(double));
+        C[i] = malloc(N * sizeof(double));
+    }
+
+    double start, end;
+
+    for (i = 0; i < M; i++)
+    {
+        for (j = 0; j < N; j++)
+        {
+            A[i][j] = j * 1;
+            B[i][j] = i * j + 2;
+            C[i][j] = j - i * 2;
+        }
+    }
+
+    start = omp_get_wtime();
+
+    for (i = 0; i < M; i++)
+    {
+        for (j = 0; j < N; j++)
+        {
+            sum = 0;
+            for (k = 0; k < M; k++)
+            {
+                sum += A[i][k] * B[k][j];
+            }
+            C[i][j] = sum;
+        }
+    }
+
+    end = omp_get_wtime();
+
+    printf("Time of computation: %f\n", end - start);
+}

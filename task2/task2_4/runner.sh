e977b0cf77a0970fbb0e2f8785a9f90ec3c0bf31
Juan Alvarado <jalvarado91@gmail.com>
2017-07-09 19:36:25 -0400

    1 cpu data

--- task2/task2_4/runner.sh
+++ task2/task2_4/runner.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 #THREADS=1
-TRIALS=1
+TRIALS=10
 
-threadcount=(32 8 16 4 1)
+threadcount=(32 16 8 4 1)
 
 for tcount in "${threadcount[@]}"
 do
@@ -12,7 +12,7 @@ do
 	for ((t=1; t<=$TRIALS; t++))
 	do
 		echo "Running Trial $t"
-		./task2_4_both $tcount > out/2cpu/both_5000x5000_${tcount}th_trial$t
+		./task2_4_both $tcount > out/1cpu/both_50x50_${tcount}th_trial$t
 	done
 
 	echo "Done Running Trials for $tcount threads."
